acf22ab5b7e0c9de30bd1a9ebab3f07ce400788c
caioliveira <caio-henrique718@outlook.com>
2022-09-07 09:37:10 -0300

    feat: new dist version based in dev changes

---
+++ dist/config/plugins.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ env }) => ({
+    'editorjs': {
+        enabled: true,
+        resolve: './src/plugins/strapi-plugin-react-editorjs'
+    },
+});
