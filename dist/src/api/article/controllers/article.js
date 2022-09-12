e3570eba1fa19f81aa6cee14574758dc995d1c69
caioliveira <caio-henrique718@outlook.com>
2022-09-12 20:56:31 -0300

    feat: customize route to get articles

--- dist/src/api/article/controllers/article.js
+++ dist/src/api/article/controllers/article.js
@@ -4,4 +4,18 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::article.article');
+exports.default = strapi_1.factories.createCoreController("api::article.article", () => ({
+    async find(ctx) {
+        const fieldsToRemove = new URLSearchParams(ctx.request.url).get("!fields").split(",") || [];
+        const { data, meta } = await super.find(ctx);
+        //TODO: custom parameter to remove selected fields from the response
+        //TODO: example: http://...?page=1&!fields=content,title
+        const result = data.map((item) => {
+            Object.keys(item.attributes).forEach((key) => {
+                fieldsToRemove.includes(key) ? delete item.attributes[key] : null;
+            });
+            return item;
+        });
+        return { data: result, meta };
+    },
+}));
--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -2,6 +2,23 @@
  * article controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::article.article');
+export default factories.createCoreController("api::article.article", () => ({
+  async find(ctx) {
+    const fieldsToRemove =
+      new URLSearchParams(ctx.request.url).get("!fields").split(",") || [];
+    const { data, meta } = await super.find(ctx);
+
+    //TODO: custom parameter to remove selected fields from the response
+    //TODO: example: http://...?page=1&!fields=content,title
+    const result = data.map((item) => {
+      Object.keys(item.attributes).forEach((key) => {
+        fieldsToRemove.includes(key) ? delete item.attributes[key] : null;
+      });
+      return item;
+    });
+
+    return { data: result, meta };
+  },
+}));
