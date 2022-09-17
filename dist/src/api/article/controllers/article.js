500e021d19bbf623663349c7f03f715c9cc2c689
caioliveira <caio-henrique718@outlook.com>
2022-09-17 13:13:51 -0300

    chore: change articles controller to return average rating

--- dist/src/api/article/controllers/article.js
+++ dist/src/api/article/controllers/article.js
@@ -6,16 +6,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
 exports.default = strapi_1.factories.createCoreController("api::article.article", () => ({
     async find(ctx) {
-        const fieldsToRemove = new URLSearchParams(ctx.request.url).get("!fields").split(",") || [];
-        const { data, meta } = await super.find(ctx);
-        //TODO: custom parameter to remove selected fields from the response
-        //TODO: example: http://...?page=1&!fields=content,title
-        const result = data.map((item) => {
+        var _a;
+        const fieldsToRemove = ((_a = new URLSearchParams(ctx.request.url).get("!fields")) === null || _a === void 0 ? void 0 : _a.split(",")) || [];
+        const { data: articles, meta } = await super.find(ctx);
+        let formatedArticles = articles;
+        // custom parameter to remove selected fields from the response
+        // example: http://...?page=1&!fields=content,title
+        formatedArticles = articles.map((item) => {
             Object.keys(item.attributes).forEach((key) => {
                 fieldsToRemove.includes(key) ? delete item.attributes[key] : null;
             });
             return item;
         });
-        return { data: result, meta };
+        const feedbacks = await strapi.entityService.findMany("api::feedback.feedback", {
+            select: ["rating"],
+            populate: {
+                article: {
+                    fields: ["id"],
+                    where: {
+                        $in: articles.map((article) => article.id),
+                    },
+                },
+            },
+        });
+        formatedArticles = formatedArticles.map((article) => {
+            let totalRatings = null;
+            let quantity = 0;
+            feedbacks.forEach((feedback) => {
+                if (feedback.article.id === article.id) {
+                    totalRatings += feedback.ratingPoints;
+                    quantity += 1;
+                }
+            });
+            return {
+                ...article,
+                feedbacks: {
+                    averageRatings: totalRatings === null ? null : totalRatings / quantity,
+                },
+            };
+        });
+        return { data: formatedArticles, meta };
     },
 }));
