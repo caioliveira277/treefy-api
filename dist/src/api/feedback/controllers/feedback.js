794e15889bb37a8cf362edf8d456e84054747e3a
caioliveira <caio-henrique718@outlook.com>
2022-09-17 13:13:27 -0300

    chore: add feedbacks endpoint

---
+++ dist/src/api/feedback/controllers/feedback.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * feedback controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::feedback.feedback');
